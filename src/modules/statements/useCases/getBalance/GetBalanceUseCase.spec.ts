bbf9abc5caf1b171408a5ee4b492ec09e85a533f
gabrielrsp <gabrielrsp18@gmail.com>
2022-01-30 22:18:05 -0300

    create balance test

--- src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -1,15 +1,14 @@
 import 'reflect-metadata';
 import { InMemoryStatementsRepository } from '../../repositories/in-memory/InMemoryStatementsRepository'
 
-import { AppError } from '../../../../shared/errors/AppError'
-
-import { CreateStatementUseCase } from './CreateStatementUseCase'
-import { CreateStatementError } from './CreateStatementError'
+import { GetBalanceUseCase } from './GetBalanceUseCase'
+import { GetBalanceError } from './GetBalanceError'
+import { CreateStatementUseCase } from '../createStatement/CreateStatementUseCase'
 import { InMemoryUsersRepository } from '../../../users/repositories/in-memory/InMemoryUsersRepository'
 import { OperationType } from '@modules/statements/entities/Statement'
 
 
-
+let getBalanceUseCase: GetBalanceUseCase
 let createStatementUseCase: CreateStatementUseCase
 let inMemoryStatementsRepository: InMemoryStatementsRepository
 let inMemoryUsersRepository: InMemoryUsersRepository
@@ -19,64 +18,53 @@ describe("Create Statement", () => {
 
   beforeAll(async () => {
     inMemoryUsersRepository = new InMemoryUsersRepository()
+
     user = await inMemoryUsersRepository.create({
       name: 'test',
       email: 'test@test.com',
       password: 'test'
     })
 
+
   })
 
   beforeEach(async () => {
     inMemoryStatementsRepository = new InMemoryStatementsRepository();
+    getBalanceUseCase = new GetBalanceUseCase(inMemoryStatementsRepository, inMemoryUsersRepository)
     createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository)
   })
 
-  it("should be able to create a new statement of type deposit", async () => {
-    const statement = await createStatementUseCase.execute({
-      user_id: user.id,
-      type: OperationType.DEPOSIT,
-      amount: 10,
-      description: 'deposit test 10 bucks'
-    })
 
-    expect(statement).toHaveProperty("id");
-    expect(statement.type).toEqual("deposit")
-
-  });
-
-  it("should be able to create a new statement of type withdraw", async () => {
+  it("should be able to show the total balance from all the statements", async () => {
     await createStatementUseCase.execute({
       user_id: user.id,
       type: OperationType.DEPOSIT,
-      amount: 10,
+      amount: 20,
       description: 'deposit test 10 bucks'
     })
 
-    const statement = await createStatementUseCase.execute({
+    await createStatementUseCase.execute({
       user_id: user.id,
       type: OperationType.WITHDRAW,
-      amount: 10,
+      amount: 5,
       description: 'withdraw test 10 bucks'
     })
 
-    expect(statement).toHaveProperty("id");
-    expect(statement.type).toEqual("withdraw")
+
+    const result = await getBalanceUseCase.execute({
+      user_id: user.id
+    })
+
+    expect(result).toHaveProperty("statement")
+    expect(result).toHaveProperty("balance")
+    expect(result.balance).toEqual(15)
+
+
+    // expect(statement).toHaveProperty("id");
+    // expect(statement.type).toEqual("withdraw")
 
   });
 
-
-  it("should not be able to create a new statement of type withdraw if there is insufficient funds", async () => {
-    expect(async () => {
-      await createStatementUseCase.execute({
-        user_id: user.id,
-        type: OperationType.WITHDRAW,
-        amount: 10,
-        description: 'withdraw test 10 bucks'
-      })
-
-    }).rejects.toBeInstanceOf(CreateStatementError.InsufficientFunds);
-  });
 
 });
 
