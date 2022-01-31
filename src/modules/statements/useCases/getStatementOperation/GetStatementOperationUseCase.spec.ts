50058d203da1645fc25e396b07d1081af84a395a
gabrielrsp <gabrielrsp18@gmail.com>
2022-01-30 22:45:18 -0300

    create test to get specific statement

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -1,67 +1,54 @@
 import 'reflect-metadata';
 import { InMemoryStatementsRepository } from '../../repositories/in-memory/InMemoryStatementsRepository'
 
-import { GetBalanceUseCase } from './GetBalanceUseCase'
-import { GetBalanceError } from './GetBalanceError'
+import { AppError } from '../../../../shared/errors/AppError'
+
+import { GetStatementOperationUseCase } from './GetStatementOperationUseCase'
 import { CreateStatementUseCase } from '../createStatement/CreateStatementUseCase'
 import { InMemoryUsersRepository } from '../../../users/repositories/in-memory/InMemoryUsersRepository'
 import { OperationType } from '@modules/statements/entities/Statement'
 
 
-let getBalanceUseCase: GetBalanceUseCase
+let getStatementOperationUseCase: GetStatementOperationUseCase
 let createStatementUseCase: CreateStatementUseCase
 let inMemoryStatementsRepository: InMemoryStatementsRepository
 let inMemoryUsersRepository: InMemoryUsersRepository
 let user
 
-describe("Get Balance", () => {
+describe("Get Statement", () => {
 
   beforeAll(async () => {
     inMemoryUsersRepository = new InMemoryUsersRepository()
-
     user = await inMemoryUsersRepository.create({
       name: 'test',
       email: 'test@test.com',
       password: 'test'
     })
 
-
   })
 
   beforeEach(async () => {
     inMemoryStatementsRepository = new InMemoryStatementsRepository();
-    getBalanceUseCase = new GetBalanceUseCase(inMemoryStatementsRepository, inMemoryUsersRepository)
     createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository)
+    getStatementOperationUseCase = new GetStatementOperationUseCase(inMemoryUsersRepository, inMemoryStatementsRepository)
   })
 
-
-  it("should be able to show the total balance from all the statements", async () => {
-    await createStatementUseCase.execute({
+  it("should be able to get an specific statement", async () => {
+    const newStatement = await createStatementUseCase.execute({
       user_id: user.id,
       type: OperationType.DEPOSIT,
-      amount: 20,
+      amount: 10,
       description: 'deposit test 10 bucks'
     })
 
-    await createStatementUseCase.execute({
+    const result = await getStatementOperationUseCase.execute({
       user_id: user.id,
-      type: OperationType.WITHDRAW,
-      amount: 5,
-      description: 'withdraw test 10 bucks'
+      statement_id: newStatement.id
     })
 
-
-    const result = await getBalanceUseCase.execute({
-      user_id: user.id
-    })
-
-    expect(result).toHaveProperty("statement")
-    expect(result).toHaveProperty("balance")
-    expect(result.balance).toEqual(15)
-
-
-    // expect(statement).toHaveProperty("id");
-    // expect(statement.type).toEqual("withdraw")
+    expect(result).toHaveProperty("id");
+    expect(result.type).toEqual("deposit")
+    expect(result.amount).toEqual(10)
 
   });
 
