7d23b5fe4a891613744c88b816bbc30c8847c8ee
gabrielrsp <gabrielrsp18@gmail.com>
2022-03-20 20:50:57 -0300

    adjust database connection settings

--- src/database/index.ts
+++ src/database/index.ts
@@ -19,3 +19,18 @@ export default async (host = "database"): Promise<Connection> => {
 if (process.env.NODE_ENV !== "test") {
   (async () => await createConnection())();
 }
+
+
+/**
+ * Your DATABASE_URL refers to 127.0.0.1, which is the loopback adapter (more here). This means "connect to myself".
+
+When running both applications (without using Docker) on the same host, they are both addressable on the same adapter (also known as localhost).
+
+When running both applications in containers they are not both on localhost as before.
+Instead you need to point the web container to the db container's IP address on the docker0 adapter - which docker-compose sets for you.
+
+Change:
+
+127.0.0.1 to CONTAINER_NAME (e.g. db)
+
+ */
